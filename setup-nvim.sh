0fcaf9cb49826c158a91dc18cef6d04dbb8a0e86
DamascusSmith <wickendenwilliam@gmail.com>
2026-06-23 10:10:40 -0400

    Telescope added

---
+++ setup-nvim.sh
@@ -0,0 +1,6 @@
+# in future will write a file to auto setup nvim
+#
+# List of things I've done for automating later
+#
+# - Install ripgrep(For telescopes live_grep)
+# I've done this via apt install ripgrep in this case
