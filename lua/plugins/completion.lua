9fc483acf7cdbd73204ee69cf49ebd17c1f97caa
DamascusSmith <wickendenwilliam@gmail.com>
2026-06-30 11:09:14 -0400

    Save latest nvim config

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -23,8 +23,21 @@ return {
         },
 
         mapping = cmp.mapping.preset.insert({
-          ["<C-n>"] = cmp.mapping.select_next_item(),
-          ["<C-p>"] = cmp.mapping.select_prev_item(),
+          ["<Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_next_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+						if cmp.visible() then
+							cmp.select_prev_item()
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
 
           ["<C-Space>"] = cmp.mapping.complete(),
 
